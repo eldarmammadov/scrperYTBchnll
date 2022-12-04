797f5c0c6ca07bd5d25d59e1c6a24f1e769f165b
eldarmammadov <eldar_mammadov@hotmail.com>
2022-12-04 23:06:20 +0400

    pulling data of categories

--- scrpYTBchnl_v001.00.py
+++ scrpYTBchnl_v001.00.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
+from selenium.webdriver.support.ui import Select
+
+from tkinter import *
+
 # going to url(amzn) via Selenium WebDriver
 chrome_options = Options()
 chrome_options.headless = False
@@ -23,9 +27,68 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get('https://channelcrawler.com/')
 
 #take category values
-
 #show them to select one of them
 drp_menu=driver.find_elements(By.XPATH,'//label[@for="inputGenre"]/parent::div//div[@class="dropdown-main"]/ul/li')
 print(len(drp_menu))
+ls_categories=[]
+ls_data_value=[]
 for i in drp_menu:
-    print(i.get_attribute('innerText'))
+    ls_categories.append(i.get_attribute('innerText'))
+    ls_data_value.append(i.get_attribute('data-value'))
+
+print(ls_categories)
+print(ls_data_value)
+d_selectelements=dict(zip(ls_data_value,ls_categories))
+print(d_selectelements)
+
+# Create object
+root = Tk()
+
+# Adjust size
+root.geometry( "200x200" )
+
+#send keys
+slect_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//div[@class='dropdown-display-label']"))).click()
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value='23']"))).click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value='1']"))).click()
+
+def fsend_keys(val_selected):
+    slect_element=Select(driver.find_element(By.XPATH,'//label[@for="inputGenre"]/parent::div//select[@placeholder="Choose a Category"]'))
+    for key, value in d_selectelements.items():
+        if value==val_selected:
+            print(value,type(value))
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value="+key+"]"))).click()
+
+
+def choose():
+    fsend_keys(clicked.get())
+
+# Change the label text
+def show():
+	label.config( text = clicked.get() )
+
+# Dropdown menu options
+options = ls_categories
+
+# datatype of menu text
+clicked = StringVar()
+
+# initial menu text
+clicked.set( "None" )
+
+# Create Dropdown menu
+drop = OptionMenu( root , clicked , *options )
+drop.pack()
+
+# Create button, it will change label text
+button = Button( root , text = "select" , command = show ).pack()
+button0 = Button( root , text = "choose" , command = choose ).pack()
+
+# Create Label
+label = Label( root , text = " " )
+label.pack()
+
+# Execute tkinter
+root.mainloop()
