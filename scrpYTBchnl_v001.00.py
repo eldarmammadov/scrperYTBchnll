a16ba0023ef2be8e2be6fba249c016a2190bc6ad
eldarmammadov <eldar_mammadov@hotmail.com>
2022-12-05 12:09:37 +0400

    two similar func for choosing with GUI <category> and <countries>

--- scrpYTBchnl_v001.00.py
+++ scrpYTBchnl_v001.00.py
@@ -41,29 +41,57 @@ print(ls_data_value)
 d_selectelements=dict(zip(ls_data_value,ls_categories))
 print(d_selectelements)
 
+#take country values
+#show them to select one of them
+drp_menuCountry=driver.find_elements(By.XPATH,'//label[@for="inputLand"]/parent::div//div[@class="dropdown-main"]/ul/li')
+print(len(drp_menuCountry))
+ls_country=[]
+ls_data_valueCountry=[]
+for i in drp_menuCountry:
+    ls_country.append(i.get_attribute('innerText'))
+    ls_data_valueCountry.append(i.get_attribute('data-value'))
+
+print(ls_country)
+print(ls_data_valueCountry)
+d_selectelementsCountry=dict(zip(ls_data_valueCountry,ls_country))
+print(d_selectelementsCountry)
+
 # Create object
 root = Tk()
 
 # Adjust size
 root.geometry( "200x200" )
 
-#send keys
-slect_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//div[@class='dropdown-display-label']"))).click()
+#select categories
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value='23']"))).click()
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value='1']"))).click()
-
-def fsend_keys(val_selected):
+def choose_categories(val_selected):
     slect_element=Select(driver.find_element(By.XPATH,'//label[@for="inputGenre"]/parent::div//select[@placeholder="Choose a Category"]'))
+    slect_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//div[@class='dropdown-display-label']"))).click()
+
     for key, value in d_selectelements.items():
         if value==val_selected:
             print(value,type(value))
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
         (By.XPATH, "//div[@class='form-group'][contains(.,'Category')]//li[@data-value="+key+"]"))).click()
 
-
 def choose():
-    fsend_keys(clicked.get())
+    choose_categories(clicked.get())
+
+
+def choose_Country(val_selected):
+    slect_element=Select(driver.find_element(By.XPATH,'//label[@for="inputLand"]/parent::div//select[@placeholder="Choose a Country"]'))
+    elementCountry = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                                 "//div[@class='form-group'][contains(.,'Countries')]//div[@class='dropdown-display-label']"))).click()
+    for key, value in d_selectelementsCountry.items():
+        if value==val_selected:
+            print(value,type(value))
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='form-group'][contains(.,'Countries')]//li[@data-value="+key+"]"))).click()
+
+def chooseCountry():
+    print(clickedCountry.get())
+    choose_Country(clickedCountry.get())
 
 # Change the label text
 def show():
@@ -71,20 +99,26 @@ def show():
 
 # Dropdown menu options
 options = ls_categories
+options2 = ls_country
 
 # datatype of menu text
 clicked = StringVar()
+clickedCountry = StringVar()
 
 # initial menu text
 clicked.set( "None" )
+clickedCountry.set( "None" )
 
 # Create Dropdown menu
 drop = OptionMenu( root , clicked , *options )
 drop.pack()
+dropCountry = OptionMenu( root , clickedCountry , *options2 )
+dropCountry.pack()
 
 # Create button, it will change label text
 button = Button( root , text = "select" , command = show ).pack()
-button0 = Button( root , text = "choose" , command = choose ).pack()
+btnChCat = Button( root , text = "choose" , command = choose ).pack()
+btnChCountry = Button( root , text = "choose Country" , command = chooseCountry ).pack()
 
 # Create Label
 label = Label( root , text = " " )
