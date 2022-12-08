dad97ca593bf7b27c4016cd7990690b09d02d7b7
eldarmammadov <eldar_mammadov@hotmail.com>
2022-12-08 15:55:13 +0400

    added interactable button for select and dropdown menu with tkinter framework for manipulating filter data of Subscription label

--- scrpYTBchnl_v001.00.py
+++ scrpYTBchnl_v001.00.py
@@ -60,6 +60,16 @@ ls_categories=[]
 for i in drp_menu:
     ls_categories.append(i.get_attribute('innerText'))
 
+drp_menu=driver.find_elements(By.XPATH,'//select[@id="queryMinSubs"]/option')
+ls_SubsMin=[]
+for i in drp_menu:
+    ls_SubsMin.append(i.get_attribute('innerText'))
+
+drp_menu=driver.find_elements(By.XPATH,'//select[@id="queryMaxSubs"]/option')
+ls_SubscMax=[]
+for i in drp_menu:
+    ls_SubscMax.append(i.get_attribute('innerText'))
+
 # Create object
 root = Tk()
 
@@ -67,17 +77,20 @@ root = Tk()
 root.geometry( "600x200" )
 
 #create frames
-frame1=Frame(root)
-frame1.grid(row=0,column=0)
+frame1=Frame(root,bg='white')
+frame1.grid(row=0,column=0,padx=3,pady=3)
 
-frame1_2=Frame(root,bg='white')
-frame1_2.grid(row=1,column=0)
+frame1_2=Frame(root,bg='red')
+frame1_2.grid(row=1,column=0,padx=3,pady=3)
 
-frame1_3=Frame(root,bg='green')
-frame1_3.grid(row=2,column=0)
+frame1_3=Frame(root,bg='#F0F0F8')
+frame1_3.grid(row=2,column=0,padx=3,pady=3)
 
 frame2=Frame(root,bg='blue')
-frame2.grid(row=0,column=1)
+frame2.grid(row=0,column=1,padx=3,pady=3)
+
+frame3=Frame(root,bg='blue')
+frame3.grid(row=0,column=2,padx=3,pady=3)
 
 #select categories
 def choose_categories(val_selected):
@@ -101,6 +114,59 @@ def choose_Country(val_selected):
 
 def chooseCountry():
     choose_Country(clickedCountry.get())
+
+def cond_minmax(sel_valMinMax):
+    print(sel_valMinMax)
+    if sel_valMinMax=='Min' or sel_valMinMax=='Max':
+        return '*[1]'
+    elif sel_valMinMax=='1,000':
+        return "option[@value='1000']"
+    elif sel_valMinMax=='100':
+        return "option[@value='100']"
+    elif sel_valMinMax=='0':
+        return "option[@value='0']"
+    elif sel_valMinMax=='200':
+        return "option[@value='200']"
+    elif sel_valMinMax=='2,000':
+        return "option[@value='2000']"
+    elif sel_valMinMax=='500':
+        return "option[@value='500']"
+    elif sel_valMinMax=='5,000':
+        return "option[@value='5000']"
+    elif sel_valMinMax=='10,000':
+        return "option[@value='10000']"
+    elif sel_valMinMax=='20,000':
+        return "option[@value='20000']"
+    elif sel_valMinMax=='50,000':
+        return "option[@value='50000']"
+    elif sel_valMinMax=='100,000':
+        return "option[@value='100000']"
+    elif sel_valMinMax=='200,000':
+        return "option[@value='200000']"
+    elif sel_valMinMax=='500,000':
+        return "option[@value='500000']"
+    elif sel_valMinMax=='1,000,000':
+        return "option[@value='1000000']"
+    elif sel_valMinMax=='2,000,000':
+        return "option[@value='2000000']"
+    elif sel_valMinMax=='5,000,000':
+        return "option[@value='5000000']"
+
+def choose_subsMin(val_selected):
+    x_ins=cond_minmax(val_selected)
+    slect_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='form-group'][contains(.,'Subscribers')]//select[@id='queryMinSubs']/"+x_ins))).click()
+
+def chsSubsMin():
+    choose_subsMin(clickedSubsMin.get())
+
+def choose_subsMax(val_selected):
+    x_ins=cond_minmax(val_selected)
+    slect_element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.XPATH, "//div[@class='form-group'][contains(.,'Subscribers')]//select[@id='queryMaxSubs']/"+x_ins))).click()
+
+def chsSubsMax():
+    choose_subsMax(clcSubscMax.get())
 
 # Change the label text
 def show():
@@ -145,33 +211,44 @@ def pd_csv():
 # Dropdown menu options
 options = ls_categories
 options2 = ls_country
+optSubsMin=ls_SubsMin
+optSubsMax=ls_SubscMax
 
 # datatype of menu text
 clicked = StringVar()
 clickedCountry = StringVar()
+clickedSubsMin=StringVar()
+clcSubscMax=StringVar()
 
 # initial menu text
 clicked.set( " None " )
 clickedCountry.set( " None " )
+clickedSubsMin.set( " None " )
+clcSubscMax.set(' None ')
 
 # Create Dropdown menu
 drop = OptionMenu( frame1 , clicked , *options )
 drop.grid(row=0,column=0)
 dropCountry = OptionMenu( frame1 , clickedCountry , *options2 )
 dropCountry.grid(row=0,column=2)
+droplbl3_02=OptionMenu(frame3,clickedSubsMin,*optSubsMin).grid(row=0,column=1,padx=3,pady=3)
+drplbl3_03=OptionMenu(frame3,clcSubscMax,*optSubsMax).grid(row=0,column=2,padx=3,pady=3)
 
 # Create button, it will change label text
-button = Button( frame1 ,width=13, text = "select" , command = show ).grid(row=2,column=0)
-btnChCat = Button( frame1 ,width=13, text = "choose Category" , command = choose ).grid(row=1,column=0)
-btnChCountry = Button( frame1 ,width=13, text = "choose Country" , command = chooseCountry ).grid(row=1,column=2)
-bntSubmit=Button(frame1_2,width=13,text="Search", command=f_submit).grid(row=0,column=0)
-bntLinks=Button(frame1_3,width=13,text="GetWebLinks", command=a_links).grid(row=0,column=0)
+button = Button( frame1 ,width=13, text = "select" , command = show ).grid(row=2,column=0,padx=3,pady=3)
+btnChCat = Button( frame1 ,width=13, text = "choose Category" , command = choose ).grid(row=1,column=0,padx=3,pady=3)
+btnChCountry = Button( frame1 ,width=13, text = "choose Country" , command = chooseCountry  ).grid(row=1,column=2,padx=3,pady=3)
+bntSubmit=Button(frame1_2,width=13,text="Search", command=f_submit ).grid(row=0,column=0,padx=3,pady=3)
+bntLinks=Button(frame1_3,width=13,text="GetWebLinks", command=a_links ).grid(row=0,column=0,padx=3,pady=3)
+bnt_SubsMin=Button(frame3,width=13,text="chooseMinValue", command=chsSubsMin ).grid(row=1,column=1,padx=3,pady=3)
+bnt_SubsMax=Button(frame3,width=13,text="chooseMaxValue", command=chsSubsMax ).grid(row=1,column=2,padx=3,pady=3)
 
 # Create Label
 label = Label( frame1 , text = " " )
-label.grid(row=1,column=1)
+label.grid(row=1,column=1,padx=3,pady=3)
 label2 = Label( frame2 , text = " " )
-label2.grid(row=4,column=0)
+label2.grid(row=4,column=0,padx=3,pady=3)
+lbl3_01=Label(frame3,width=11,bg='white',text='Subscribers').grid(row=0,column=0,padx=3,pady=3)
 
 # Execute tkinter
 root.mainloop()
